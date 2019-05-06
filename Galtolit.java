c1312ca7fc1e14e83da7d7dcbce6088ffca5207e
ayeshaaltaf <47942116+ayeshaaltaf@users.noreply.github.com>
2019-05-07 00:34:41 +0500

    git commit

---
+++ Galtolit.java
@@ -0,0 +1,11 @@
+class Galtolit{
+public static void main(string args[]) {
+  double gallons;
+  double liters;
+  
+  gallons =10;
+
+  liters =gallons*3.7854;
+  system.out.println(gallons+ "gallons is" + litters + "litters.");
+ }
+}
