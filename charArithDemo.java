df80683232b38c6c9cfb32c03a4e1d814d879a91
ayeshaaltaf <47942116+ayeshaaltaf@users.noreply.github.com>
2019-05-07 01:25:06 +0500

    git commit

---
+++ charArithDemo.java
@@ -0,0 +1,92 @@
+class GalTolitTable 
+
+
+
+
+
+class CharArithDemo {
+ public static void main(string args[]) {
+  char ch;
+
+  ch = 'X';
+  system.out.println("ch contains " + ch);
+  
+   ch++;
+   system.out.println("ch is now " + ch);
+   
+   ch=90;
+   system.out.println("ch is now " + ch);
+  }
+ }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class
