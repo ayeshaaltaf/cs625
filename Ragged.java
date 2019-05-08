3352af3f686bdd4bbc85d28f7dc43eddabe2197f
ayeshaaltaf <47942116+ayeshaaltaf@users.noreply.github.com>
2019-05-08 11:32:03 +0500

    git commit

---
+++ Ragged.java
@@ -0,0 +1,10 @@
+class Ragged
+  public static void main (string args[]) {
+   int riders[] [] = new int[7] [];
+   riders[0] = new int[10];
+   riders[1] = new int[10];
+   riders[2] = new int[10];
+   riders[3] = new int[10];
+   riders[4] = new int[10];
+   riders[5] = new int[10];
+   riders[6] = new int[10];
