f6f84893d39568e813be441c9bbd48f6326766b0
ayeshaaltaf <47942116+ayeshaaltaf@users.noreply.github.com>
2019-05-08 11:09:57 +0500

    git commit

---
+++ BlockDemo.java
@@ -0,0 +1,14 @@
+class BlockDemo {
+ public static void main(string args[]) {
+ double i, j, d;
+
+ i=5;
+ j=10;
+
+ if(i != 0) {
+  system.out.println("i does not equal zero");
+  d = j / i;
+  system.out.println("j / i is " + d);
+  }
+ }
+}
