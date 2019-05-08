c5d9c219ebc66a2fc4f2e9ce574cbef534816dbb
ayeshaaltaf <47942116+ayeshaaltaf@users.noreply.github.com>
2019-05-08 11:11:13 +0500

    git commit

---
+++ ForDemo.java
@@ -0,0 +1,10 @@
+class ForDemo {
+ public static void main(string args[]) {
+  int count;
+
+  for(count=0; count<5; count = count+1)
+    system.out.println("This is count: " + count);
+
+   system.out.println("Done!");
+ }
+}
